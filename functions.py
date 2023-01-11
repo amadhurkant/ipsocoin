e32ffd68a95e7f8bc1dee796e4a752ac629ff384
amadhurkant <85044482+amadhurkant@users.noreply.github.com>
2023-01-11 19:42:59 +0530

    Added MultiKeyWallets and history
    For details of this commit check history of: Maagh-K-5-2079

--- functions.py
+++ functions.py
@@ -2,24 +2,18 @@ import basicCalc as bc
 import base58 as b58
 import hashlib
 import json
-import random
+
+from random import randint
+from justpy.kfuncs import removeData, checkHx
 
 def walletGenerateSingle():
     name= input("Walltet Name: ")
-    dA = random.randint(3, bc.curve.n-2)
-    pK = bc.scalar_mult(dA, bc.curve.g)
-    pk0 = bc.scalar_mult(dA, bc.curve.g)[1]
-    if pK[1] % 2 == 0:
-        pK = "02"+removeData("0x", str(hex(pK[0])))
-    else:
-        pK = "03"+removeData("0x", str(hex(pK[0])))
-    pubh1 = hashlib.sha256(pK.encode()).hexdigest()
-    pubh2 = "66"+hashlib.new('ripemd160', pubh1.encode()).hexdigest()
-    pubh3 = hashlib.sha256((hashlib.sha256(pubh2.encode()).hexdigest()).encode()).hexdigest()
-    pubh4 = pubh3[8:16]
-    pub = pubh2+pubh4
-    pKn = b58.b58encode(bytes.fromhex("80"+removeData("0x", hex(dA)))).decode()
-    addr = (b58.b58encode(bytes.fromhex(pub))).decode()
+    dA = randint(3, bc.curve.n-2)
+    pK0 = bc.scalar_mult(dA, bc.curve.g)
+    pK = pubKeypre(pK0)
+    checksum = IpsoHasher(pK)
+    pKn = b58.b58encode(bytes.fromhex("80"+checkHx(removeData("0x", hex(dA)), 64))).decode()
+    addr = (b58.b58encode(bytes.fromhex(checksum))).decode()
     wallet_dict = {"wallet: ": name,
     "dA: ": pKn,
     "pub: ": pK,
@@ -27,16 +21,55 @@ def walletGenerateSingle():
     #file generation
     with open(name+".json", 'w') as outfile:
         json.dump(wallet_dict, outfile)
-    print("wallet file created - pls check")
+    print("wallet file created")
 
-def removeData(remove, data):
-    if remove not in data:
-        raise Exception(f"RemoveNotError: Data to be removed- {remove} -not found in provided data")
-    occurence = data.count(remove)
-    if remove in data:
-        data0 =  data.split("0x", occurence)
-    data2 = ''
-    for i in data0:
-        data2 = data2+i
-    return data2
-print(walletGenerateSingle())
+def walletGenerateMulti():
+    users = int(input("Enter number of users: "))
+    name = input("Enter base name for wallet generation: ")
+    usr_keys_basic =[randint(3, bc.curve.n-2) for i in range(0, users)]
+    usr_keys_priv = []
+    dump = []
+    usr_keys_pub = []
+    # Loops for private wif formation and public key calculations
+    for i in usr_keys_basic:
+        usr_keys_priv.append(b58.b58encode(bytes.fromhex("80"+checkHx(removeData("0x", hex(i)), 64))).decode())
+    for i in usr_keys_basic:
+        a = bc.scalar_mult(i, bc.curve.g)
+        dump.append(a)
+        usr_keys_pub.append(pubKeypre(a))
+    # Code Continues
+    master_pub_key = pubKeypre(keyAdd(dump))
+    checksum = IpsoHasher(master_pub_key)
+    addr = b58.b58encode(bytes.fromhex(checksum)).decode()
+    usr_wallets = []
+    # Creates Multiple File
+    for usr in range(0, users):
+        usr_wallets.append({"wallet: ": name+" "+str(usr), "dA: ": usr_keys_priv[usr], "pub: ": master_pub_key, "address: ": addr})
+    for usr in range(0, len(usr_wallets)):
+        with open(name+" "+str(usr)+".json", 'w') as outfile:
+            json.dump(usr_wallets[usr], outfile)
+    print("Wallet files created")
+
+def pubKeypre(pK):
+    if pK[1] % 2 == 0:
+        pK = "02"+checkHx(removeData("0x", (hex(pK[0]))), 64)
+    else:
+        pK = "03"+checkHx(removeData("0x", (hex(pK[0]))), 64)
+    return pK
+
+def keyAdd(keylist):
+    ne = []
+    for n in keylist:
+        if len(ne) == 0:
+            ne.append(n)
+        else:
+            ne.append(bc.point_add(ne.pop(), n))
+    return ne[-1]
+
+def IpsoHasher(pK):
+    pubh1 = hashlib.sha256(pK.encode()).hexdigest()
+    pubh2 = "66"+hashlib.new('ripemd160', pubh1.encode()).hexdigest()
+    pubh3 = hashlib.sha256((hashlib.sha256(pubh2.encode()).hexdigest()).encode()).hexdigest()
+    pubh4 = pubh3[8:16]
+    pub = pubh2+pubh4
+    return pub
